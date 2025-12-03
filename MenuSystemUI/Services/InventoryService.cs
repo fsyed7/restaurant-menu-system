3fc1841cf5b2119a4498a8858b6d3879cfcd6d80
Thffe <90644494+Thffe@users.noreply.github.com>
2025-12-03 13:29:43 -0500

    updated inventory service receiver

--- MenuSystemUI/Services/InventoryService.cs
+++ MenuSystemUI/Services/InventoryService.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MenuSystemUI.Models;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MenuSystemUI.Services
 {
@@ -42,5 +44,39 @@ namespace MenuSystemUI.Services
 
             return false;
         }
+
+        public async Task<bool> getMenu(Models.Menu menu) {
+            string apiUrl = ""; // TODO: Inventory endpoint from Inventory team
+
+            if (string.IsNullOrWhiteSpace(apiUrl)) {
+                //Console.WriteLine($"[InventoryService] Would update inventory for FoodId={foodId}, Qty={quantitySold} (no API configured yet).");
+                await Task.CompletedTask;
+                return true;
+            }
+
+            try {
+                int foodId, cals, quantity;
+                string name, category, desc;
+                double price;
+                string json = $"{{\"foodId\": {foodId}, \"Name\":{name}, \"Category\":{category}, \"Price\":{price}, \"Quantity\":{quantity}, \"NutStatsCal\":{cals}, \"Description\":{desc}}}";
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode) {
+                    Console.WriteLine("Inventory successfully updated in Inventory Management System!");
+                    FoodItem food = new FoodItem(foodId,name,category, price, quantity, cals, desc, "\\src\\testimage.jpg");
+                    menu.Items.Add(food);
+                    return true;
+                }
+
+                Console.WriteLine($"Inventory API returned error: {response.StatusCode}");
+            } catch (Exception ex) {
+                Console.WriteLine($"Failed to update inventory: {ex.Message}");
+            }
+
+            return false;
+        }
+
     }
 }
