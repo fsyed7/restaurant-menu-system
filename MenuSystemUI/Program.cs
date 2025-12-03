9ff527c7056fcfd8966dc18c6198d10f89df9d72
Bean <145775095+Padole05@users.noreply.github.com>
2025-12-03 12:59:19 -0500

    Configure HttpClient for TransactionService
    Added HttpClient configuration for TransactionService with placeholder for team IP.

--- MenuSystemUI/Program.cs
+++ MenuSystemUI/Program.cs
@@ -9,6 +9,18 @@ builder.Services.AddSingleton<IMenuDataService, SampleDataService>();
 builder.Services.AddSingleton<CartService>();
 builder.Services.AddSingleton<DatabaseService>();
 builder.Services.AddSingleton<InventoryService>();
+//for transaction
+/*
+ if transaction team server is 10.0.0.25
+  client.BaseAddress = new Uri("http://10.0.0.25:23500");
+    or hostname
+client.BaseAddress = new Uri("http://transactionhostname:23500");
+
+ */
+builder.Services.AddHttpClient<TransactionService>(client =>
+{
+    client.BaseAddress = new Uri("http://<TRANSACTION_TEAM_IP>:23500");
+});
 
 var app = builder.Build();
 
